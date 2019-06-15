b1be399c200c380aaa6e8eb21ec4fe4fefd55f08
Ted Selig <edwardrselig@gmail.com>
2019-06-15 18:10:54 -0400

    fix shiva bug

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -11,7 +11,7 @@ const initialState = {
     recs: []
 };
 
-export function main(state = initialState, action) {
+export default function main(state = initialState, action) {
     switch (action.type) {
         case FETCH_USER:
             return {
